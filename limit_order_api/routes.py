c49e24cdd38c27b249b63c4067379d9f599ae5cc
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-04-08 00:03:22 +0530

    feat: implement modify order

--- limit_order_api/routes.py
+++ limit_order_api/routes.py
@@ -1,18 +1,42 @@
 from fastapi import APIRouter, HTTPException
-from limit_order_api.db import database
-from limit_order_api.models import Order, OrderRequest
+from limit_order_api.db import SessionLocal
+from limit_order_api.models import Order, PlaceOrderRequest, ModifyOrderRequest
 
 router = APIRouter()
 
 
 @router.post("/place_order")
-async def place_order(order: OrderRequest):
+async def place_order(order: PlaceOrderRequest):
     quantity = order.quantity
     price = order.price
     side = order.side
+
     if quantity <= 0 or price <= 0 or side not in [-1, 1]:
         raise HTTPException(status_code=400, detail="Invalid input")
 
-    query = Order.__table__.insert().values(quantity=quantity, price=price, side=side)
-    order_id = await database.execute(query)
-    return {"order_id": order_id}
+    new_order = Order(quantity=quantity, price=price, side=side)
+
+    with SessionLocal() as session:
+        session.add(new_order)
+        session.commit()
+
+    return {"order_id": new_order.id}
+
+
+@router.put("/modify_order")
+async def modify_order(order: ModifyOrderRequest):
+    order_id = order.order_id
+    updated_quantity = order.updated_quantity
+    updated_price = order.updated_price
+
+    with SessionLocal() as session:
+        order = session.get(Order, order_id)
+        if order is None or updated_quantity <= 0 or updated_price <= 0:
+            return {"success": False}
+
+        updated_order = order
+        updated_order.quantity = updated_quantity
+        updated_order.price = updated_price
+        session.commit()
+
+    return {"success": True}
