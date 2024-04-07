fc87ebb710f250738aa3d4a91e973dc697ac5d4b
Abhishek Keshri <iam2kabhishek@gmail.com>
2024-04-07 18:15:24 +0530

    feat: add openapi docs endpoint

--- limit_order_api/main.py
+++ limit_order_api/main.py
@@ -1,7 +1,26 @@
 from fastapi import FastAPI
 from uvicorn import run
+from fastapi.openapi.utils import get_openapi
+
 
 app = FastAPI()
+
+
+def custom_openapi():
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="limit-order-api",
+        version="0.1.0",
+        description="API to demonstrate limit order book functionality",
+        routes=app.routes,
+    )
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
+
 
 def start():
     run("limit_order_api.main:app", host="0.0.0.0", port=8000, reload=True)
